ec495e87d3fc05e1413e0f29a6bc4a2fbeae2999
bitcooker <dreamhighpassionhigh@gmail.com>
2023-04-04 23:24:43 -0400

    feat: finishing up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '@/components/layouts/Layout';
+import Dashboard from '@/components/modules/Dashboard';
 
 const Home = () => {
   return (
     <Layout>
-      <h3>Hello</h3>
+      <Dashboard />
     </Layout>
   );
 };
